9f7b766289037f65d500bb6bfcf9775f7ffe8843
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2021-12-12 11:25:28 +0000

    coming soon banner

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
+        <h1 className="hero__title">coming soon ...</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
     </header>
